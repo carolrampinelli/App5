d9f865cba35512b25f05fea5b7327f1ff9e3c941
Ana Carolina Carvalho Cassiano <20181ENEL0037@cefetes.br>
2019-03-26 10:30:59 -0300

    Versão estruturada.

--- App5/App5/MainPage.xaml.cs
+++ App5/App5/MainPage.xaml.cs
@@ -7,11 +7,39 @@ using Xamarin.Forms;
 
 namespace App5
 {
-	public partial class MainPage : ContentPage
-	{
-		public MainPage()
-		{
-			InitializeComponent();
-		}
-	}
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        void OnButtonClicked(object sender, EventArgs args)
+        {
+            Label1.IsVisible = false;
+            Label2.IsVisible = false;
+
+            if (Entry2.Text != null &&
+                Entry2.Text.Length > 0 &&
+                Plicker.SelectedIndex >= 0)
+            {
+                int periodo = int.Parse(Entry.2Text);
+                int semestre = Picker.SelectedIndex + 1;
+
+                if (periodo == semestre)
+                {
+                    Label1.IsVisible = true;
+                }
+                else
+                {
+                    Label2.IsVisible = true;
+                }
+            }
+            else
+            {
+                Label2.IsVisible = true;
+            }
+
+        }
+    }
 }
